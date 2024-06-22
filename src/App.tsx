2a45c7b544e430628fde2f76b8a7db94615dd0ec
phan-forsure <phantommy38@gmail.com>
2024-06-23 02:58:42 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './style/app.css';
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Islamic Prayer Times Application</h1>
+    </div>      
+  )
+}
+
+export default App
