06747a34ebacb2503c7b3a0e816d8a7da76d948c
phan-forsure <phantommy38@gmail.com>
2024-06-23 04:36:21 +0300

    added input function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
+import Input from './components/Input';
 import './style/app.css';
 function App() {
-
   return (
     <div className="app">
       <h1>Islamic Prayer Times Application</h1>
-    </div>      
+      <Input />
+    </div>
   )
 }
 
