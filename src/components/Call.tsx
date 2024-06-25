ecb6bf1ac5afbb0e7599c129228df7d3e2450107
phan-forsure <phantommy38@gmail.com>
2024-06-25 06:39:06 +0300

    updated + bug fixes

--- src/components/Call.tsx
+++ src/components/Call.tsx
@@ -4,7 +4,7 @@ import Output from './Output'
 
 export default function Call() {
     const cityRef = React.useRef<HTMLInputElement>(null)
-    const countryRef = React.useRef<HTMLInputElement>(null)
+    // const countryRef = React.useRef<HTMLInputElement>(null)
     
     const prayerTimes = useQuery({ queryKey: ['times'], queryFn: () => fetchPrayerTimes(), enabled: false, })
     const location = useQuery({ queryKey: ['location'], queryFn: () => fetchLocation(), enabled: true, })
@@ -19,7 +19,7 @@ export default function Call() {
 
     async function fetchPrayerTimes() {
         const date = new Date()
-        const data = await fetch(`http://api.aladhan.com/v1/calendarByCity/${date.getFullYear()}/${date.getMonth() + 1}?city=${cityRef.current.value}&country=${countryRef.current.value}`)
+        const data = await fetch(`https://api.aladhan.com/v1/calendarByCity/${date.getFullYear()}/${date.getMonth() + 1}?city=${cityRef.current.value}&country=`)
         if (!data.ok) {
             console.log(data.status)
         }
@@ -37,7 +37,7 @@ export default function Call() {
         <>
         <section className='input-data comp'>
             <form className='flex justify-center flex-wrap content-center' name="city" id="cityform">
-                <input type="text" placeholder="الدوله" ref={countryRef} defaultValue={location.data?.countryName}/>
+                {/* <input type="text" placeholder="الدوله" ref={countryRef} defaultValue={location.data?.countryName}/> */}
                 <input type="text" placeholder="المدينة" ref={cityRef} defaultValue={location.data?.city}/>
                 <input className='w-full bg-slate-600 p-5 rounded hover:bg-slate-500 transition-all my-8 cursor-pointer' type="submit" value="ابحث" onClick={handleClick}/>
             </form>
